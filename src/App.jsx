20dbf245ceb9b1639e1484b71ef37b2b791da314
Albert Augusto <augustothelema@gmail.com>
2024-09-08 18:56:39 -0300

    1 REACT

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 function App() {
-   return (
-      <>
-
-      </>
-   );
+  const atividade = {
+    name: "Albert",
+    module: "Introdução ao REACT",
+    age: "33",
+  };
+  const alerta = () =>{
+    alert(atividade.name);
+  }
+  return (
+    <>
+      <li>
+        <h3>{atividade.name}</h3>
+        <h4>{atividade.module}</h4>
+        <p>{atividade.age}</p>
+      </li>
+      <button onClick={alerta}>QUEM SOU EU</button>
+    </>
+  )
 }
 
-export default App;
+export default App
